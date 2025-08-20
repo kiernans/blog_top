9b277d8508105e0d55a0de3dfd1b215668e732ce
Kiernan Soriano <nukabunny@gmail.com>
2025-08-19 17:45:48 -0700

    Removed unneeded authenticate call

--- apps/api/src/routes/users.ts
+++ apps/api/src/routes/users.ts
@@ -1,25 +1,18 @@
 import { Router } from 'express';
 import { prisma } from '@lib/prisma';
-import passport from 'passport';
 
 const router = Router();
 
-router.get(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    const users = await prisma.user.findMany({
-      select: {
-        id: true,
-        email: true,
-        name: true,
-      },
-    });
+router.get('/', async (req, res) => {
+  const users = await prisma.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      name: true,
+    },
+  });
 
-    console.log(req.user);
-
-    return res.json(users);
-  },
-);
+  return res.json(users);
+});
 
 export default router;
