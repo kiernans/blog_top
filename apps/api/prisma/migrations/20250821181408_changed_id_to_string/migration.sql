56ed1360b3b12b3aea22f66ebb48856c05df3e98
Kiernan Soriano <nukabunny@gmail.com>
2025-08-21 11:22:05 -0700

    Updated ids to be string

---
+++ apps/api/prisma/migrations/20250821181408_changed_id_to_string/migration.sql
@@ -0,0 +1,61 @@
+/*
+  Warnings:
+
+  - The values [ADMIN] on the enum `Role` will be removed. If these variants are still used in the database, this will fail.
+  - The primary key for the `Comment` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "public"."Role_new" AS ENUM ('USER', 'AUTHOR');
+ALTER TABLE "public"."User" ALTER COLUMN "role" DROP DEFAULT;
+ALTER TABLE "public"."User" ALTER COLUMN "role" TYPE "public"."Role_new" USING ("role"::text::"public"."Role_new");
+ALTER TYPE "public"."Role" RENAME TO "Role_old";
+ALTER TYPE "public"."Role_new" RENAME TO "Role";
+DROP TYPE "public"."Role_old";
+ALTER TABLE "public"."User" ALTER COLUMN "role" SET DEFAULT 'USER';
+COMMIT;
+
+-- DropForeignKey
+ALTER TABLE "public"."Comment" DROP CONSTRAINT "Comment_postId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Comment" DROP CONSTRAINT "Comment_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."Post" DROP CONSTRAINT "Post_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "public"."Comment" DROP CONSTRAINT "Comment_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "postId" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Comment_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Comment_id_seq";
+
+-- AlterTable
+ALTER TABLE "public"."Post" DROP CONSTRAINT "Post_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "Post_id_seq";
+
+-- AlterTable
+ALTER TABLE "public"."User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+DROP SEQUENCE "User_id_seq";
+
+-- AddForeignKey
+ALTER TABLE "public"."Post" ADD CONSTRAINT "Post_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Comment" ADD CONSTRAINT "Comment_postId_fkey" FOREIGN KEY ("postId") REFERENCES "public"."Post"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "public"."Comment" ADD CONSTRAINT "Comment_userId_fkey" FOREIGN KEY ("userId") REFERENCES "public"."User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
